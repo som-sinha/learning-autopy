32366bdff708bd1878c42491a8a7bac17ba0256b
som-sinha <somaditya29.s@gmail.com>
2019-09-24 00:15:38 +0530

    Minor formatting changes

--- firstprogram.py
+++ firstprogram.py
@@ -1,10 +1,9 @@
 # This program says hello and asks for my name
 print('Hello World')
 
-myName = input('What is your name?')        # Ask for name and store it
+myName = input('What is your name? ')        # Ask for name and store it
 print('It is good to meet you, ' + myName)
-print('The length of your name is:')
-print(len(myName))
+print('The length of your name is: ' + str(len(myName)))
 
-myAge = input('What is you age?')       # Ask for their age and store it
-print('You will be ' + str(int(myAge) + 1) + 'in a year.')
+myAge = input('What is you age? ')       # Ask for their age and store it
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
