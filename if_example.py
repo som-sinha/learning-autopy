de5827b892c99c5591105592b4c2f78aaa6dc56b
som-sinha <somaditya29.s@gmail.com>
2019-09-25 23:41:01 +0530

    comment addition

--- if_example.py
+++ if_example.py
@@ -1,6 +1,6 @@
-#if statements in python
+# if statements in python
 
-name = 'Alice'
-if name == 'Alice':
-    print('Hi Alice')
+name = 'Alice
+if name == 'Alice':     # : is usually followed by a new block of code 
+    print('Hi Alice')       # indentation is used to differentiate blocks of code in programs
 print('Done')
